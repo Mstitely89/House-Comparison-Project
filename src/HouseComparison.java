b9d23f754474f88624beee75a9f723912d42c6d5
Matt <Mstitely196@myfcc.frederick.edu>
2023-05-04 11:39:32 -0400

    First Part Upload, still not quite working yet...javafx issues

--- src/HouseComparison.java
+++ src/HouseComparison.java
@@ -4,12 +4,8 @@ import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.io.*;
