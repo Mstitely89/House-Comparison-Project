1ad048136c1206d9542c28ccc6b61b4bbbb8e56d
Matt <Mstitely196@myfcc.frederick.edu>
2023-05-08 19:12:12 -0400

    Final Part 3 of Project, implemented a BinarySearchTree and Graph class, however the Graph class is not currently functional or in use. Still a work in progress.

--- src/HouseComparison.java
+++ src/HouseComparison.java
@@ -1,7 +1,7 @@
 // **********************************************************************************
 // Title: House Comparison Tool
 // Author: Matt Stitely
-// Course Section: CMIS201-ONL1 (Seidel) Spring 2023
+// Course Section: CMIS202-ONL1 (Seidel) Spring 2023
 // Description: A simple to use tool that allows the user to enter multiple
 // houses/apartments and gives the ability to compare and filter them.
 // **********************************************************************************
